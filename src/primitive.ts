9b372d8dcf763508a98db8496afba0a573eeed0b
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-11-09 21:25:38 +0600

    primitive

---
+++ src/primitive.ts
@@ -0,0 +1,17 @@
+
+// !Wrong
+// let userName: string = "mezba";
+// userName = 123
+
+// !Wrong
+// let userId = 123
+// userId = "123"
+
+// userName.repeat(2)
+// userId.toFixed()
+
+// let isAdmin: boolean = false;
+// isAdmin = true;
+
+
+// let x: undefined = undefined;
