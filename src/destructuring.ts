136a2de88e0615fc314d8c3016ff10d6432ef56e
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-11-09 22:53:48 +0600

    Destructuring in typescript

---
+++ src/destructuring.ts
@@ -0,0 +1,31 @@
+// array destructuring
+// object destructuring
+
+
+// const user = {
+//     id: 123,
+//     name: {
+//         firstName: "Mezbaul abeding",
+//         lastName: "Forhan",
+//     },
+//     gender: "male",
+//     favoriteColor: 'black'
+// }
+
+// // const myFavouriteColor = user.favoriteColor;
+// // const myLastName = user.name.lastName;
+// // console.log(myFavouriteColor,myLastName);
+
+// const {favoriteColor:color, name: {firstName:myFirstName,lastName:myLastName}} = user;
+
+// console.log(myFirstName,myLastName);
+
+
+// const friends = ["karim", "Rahim", "Mahim"];
+
+// const myBestFriends = friends[1]
+
+// const [,b,] = friends;
+
+
+// console.log(b);
