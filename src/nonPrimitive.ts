e98e971a24d624e08092f6753d7cb8d5cc684110
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-11-09 21:56:34 +0600

    Object , Literal & Optional Type

--- src/nonPrimitive.ts
+++ src/nonPrimitive.ts
@@ -17,3 +17,24 @@
 // let mezbaNameRoll: [string,number] = ["mezba", 79]
 
 // let destination: [string,string,number] = ["Dhaka","Chitogong",123]
+
+// refference type: object
+
+const user: {
+    //accessmodifire
+    readonly organization: string //value type literel types
+    // organization: string;
+    firstName: string;
+    middleName?: string; //optional type
+    lastName: string;
+    isMarried: boolean;
+} = {
+    firstName: 'jhankar',
+    lastName: 'mahbub',
+    isMarried: true,
+    organization: 'Programming hero'
+}
+
+// user.organization = "programming hero fire"
+
+console.log(user);
